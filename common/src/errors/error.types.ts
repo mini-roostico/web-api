105ee86e36fe9f045a55fa4f5c46ff3f6ecf20fa
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2025-01-13 16:01:03 +0100

    feat: set up api and common package, typescript and express added

---
+++ common/src/errors/error.types.ts
@@ -0,0 +1,8 @@
+export enum ErrorTypes {
+    LOGIN_ERROR = 1,
+    REGISTER_ERROR = 2,
+    VALIDATION_ERROR = 3,
+    JWT_ERROR = 4,
+    AUTHENTICATION_ERROR = 5,
+    GENERIC_ERROR = 6,
+}
---
+++ common/src/errors/errors.ts
@@ -0,0 +1,86 @@
+import { StatusCodes } from "http-status-codes"
+import {ErrorTypes} from "./error.types";
+
+export class HttpBaseError implements Error {
+    code: StatusCodes | number;
+    name: string;
+    message: string;
+    stack?: string | undefined;
+    type: number;
+
+    constructor(
+        code: StatusCodes | number,
+        name: string,
+        message?: string,
+        stack?: string | undefined,
+        type?: ErrorTypes
+    ) {
+        this.code = code;
+        this.name = name;
+        this.message = message? message : code.toString();
+        if(this.stack) {
+            this.stack = stack;
+        }
+        this.type = type ? type : ErrorTypes.GENERIC_ERROR;
+    }
+}
+
+export class BadRequestError extends HttpBaseError {
+    constructor(message?: string, stack?: string | undefined, type?: ErrorTypes) {
+        super(
+            StatusCodes.BAD_REQUEST,
+            "Bad Request",
+            message,
+            stack,
+            type
+        )
+    }
+}
+
+export class InternalServerError extends HttpBaseError {
+    constructor(message?: string, stack?: string | undefined, type?: ErrorTypes) {
+        super(
+            StatusCodes.INTERNAL_SERVER_ERROR,
+            "Internal Server Error",
+            message,
+            stack,
+            type
+        )
+    }
+}
+
+export class NotFoundError extends HttpBaseError {
+    constructor(message?: string, stack?: string | undefined, type?: ErrorTypes) {
+        super(
+            StatusCodes.NOT_FOUND,
+            "Not Found",
+            message,
+            stack,
+            type
+        )
+    }
+}
+
+export class UnauthorizedError extends HttpBaseError {
+    constructor(message?: string, stack?: string | undefined, type?: ErrorTypes) {
+        super(
+            StatusCodes.UNAUTHORIZED,
+            "Unauthorized",
+            message,
+            stack,
+            type
+        )
+    }
+}
+
+export class TooManyRequests extends HttpBaseError {
+    constructor(message?: string, stack?: string | undefined, type?: ErrorTypes) {
+        super(
+            StatusCodes.TOO_MANY_REQUESTS,
+            "Too Many Requests",
+            message,
+            stack,
+            type
+        )
+    }
+}
