ed09617d88a094e8108ec1a2eed689be3dd3f178
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2025-01-28 15:11:45 +0100

    feat: provided app entry point, updated gitignore and dockerfile

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,10 +1,21 @@
-import express from 'express';
+import ServerConfig from "./configs/server.configs";
 
-const app = express();
-app.use(express.json());
+const app = ServerConfig();
+
+function printInformation() {
+    console.log(`
+ __       _     _      _    _   
+/ _\\_   _| |__ (_) ___| | _| |_ 
+\\ \\| | | | '_ \\| |/ _ \\ |/ / __|
+_\\ \\ |_| | |_) | |  __/   <| |_ 
+\\__/\\__,_|_.__// |\\___|_|\\_\\\\__|
+             |__/               
+    `)
+    console.log("SPE Project - 2024/2025");
+    console.log("Authors: Francesco Magnani, Luca Rubboli");
+    console.log("Server Running on Port " + PORT);
+}
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log("Server Listening on PORT:", PORT);
-});
+app.listen(PORT, printInformation);
